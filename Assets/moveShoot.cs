40b14444ac3ca0d63fbfd7ecabac81ab9fddde66
isidro blanco anaya <isidro.blanco@uabc.edu.mx>
2016-03-01 01:28:21 -0800

    prueba1

---
+++ Assets/moveShoot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveShoot : MonoBehaviour
+{
+    public float speed = 1.0f;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += transform.rotation*((Vector3.forward).normalized * speed * Time.deltaTime);
+    }
+}
